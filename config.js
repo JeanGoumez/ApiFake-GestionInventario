9a3f61c6e563a121d35657c1c8fa7de065472349
Jean Goumez <adrielgo.mez123@gmail.com>
2025-05-23 18:24:44 -0500

    feat: Create new apifake GET/POST with validators

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 module.exports = {
   routes: {
+    //GEt
     GET_ALM: { method: 'GET' },
+    GET_CE: { method: 'GET' },
+    GET_TRAS_ALM_DOS_PASO: { method: 'GET' },
+    GET_CEN: { method: 'GET' },
+
+    //POST
     API_TRASLADO_CE_1: { method: 'POST' },
     INV_DOC: { method: 'POST' },
-    // Agrega más aquí
+    
   }
 };
